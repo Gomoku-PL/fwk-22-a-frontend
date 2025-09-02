97f1535fa28a68e37a00dfc04ad6356a0ffd3811
msharif123 <mohamed.sharif.abdiwahab@gmail.com>
2025-09-03 00:07:43 +0200

    skappade en lib map och en socket.js fil i frontend

---
+++ src/lib/socket.js
@@ -0,0 +1,26 @@
+import { io } from "socket.io-client"
+export let socket
+
+export function connect() {
+    socket = io("https://localhost:3000")  // jag måste fråga min grupp vad de har för localhost i backend på torsdag
+
+    socket.on("connet", () => {
+        console.log(" sockdt connected:", socket.id)
+    })
+}
+export function joinRoom(roomId) {
+    if (socket) {
+        socket.emit("join", roomId)
+        console.log("join room:", roomId)
+    }
+}
+export function on(event, callback) {
+    if (socket) {
+        socket.on(EventTarget, callback)
+    }
+}
+export function off(event) {
+    if (socket) {
+        socket.off(event)
+    }
+}
